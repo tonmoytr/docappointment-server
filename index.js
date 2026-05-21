ebc53c27b4038e23c5469036815898c9a142baf7
tonmoytr <tonmoytr0110@gmail.com>
2026-05-21 23:09:19 +0600

    commented out MongoDB connection and ping command; added Vercel configuration file

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("doc-appointment");
 
@@ -118,7 +118,7 @@ async function run() {
 
     // ===============================================
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
